6c19f65ad8cf42d7c6d980e36b3cdcb7f8cbca1e
Darya969 <DarinaSokolova15@mail.ru>
2023-11-16 14:16:01 +0400

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
-class PlantApp(MDApp):
+class MortgageCalculator(MDApp):
     def build(self):
         return MDLabel(text="Hello, floristry", halign="center")
 
-PlantApp().run()
+MortgageCalculator().run()
